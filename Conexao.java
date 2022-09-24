6e5c058d593ba2b273766338a4427e4f7ebf198b
Maria Clara Pires Lobo Gois <61884876+ClaraLobo19@users.noreply.github.com>
2022-09-23 23:05:51 -0300

    Update Conexao.java

--- Conexao.java
+++ Conexao.java
@@ -7,7 +7,7 @@ public class Conexao {
 	//Nome do usuário do mysql
 	private static final String USERNAME= "root";
 	//Senha do mysql
-	private static final String PASSWORD= "19231418mc";
+	private static final String PASSWORD= "********";
 	//Dados de caminho, porta e nome da base de dados que irá ser feita a conexão
 	private static final String DATABASE_URL= "jdbc:mysql://localhost:3306/BD_MYSQL";
 
