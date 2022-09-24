c515a1e0df6212461d364982ebd571e0640de3c0
Clara <mariaclaragois80@gmail.com>
2022-09-23 22:53:52 -0300

    Classe cliente Atualizada pela segunda vez

--- ClienteLogin.java
+++ ClienteLogin.java
@@ -3,106 +3,66 @@ import java.util.Date;
 
 public class ClienteLogin {
 	int ID_cliente;
-	private String Nome_Cliente;
+	private String nome_Cliente;
 	private String CPF_cliente;
 	private String RG_cliente;
-	private String telefone;
+	private String email_cliente;
+	private String telefone_Cliente;
 	private Date data_Nascimento;
-	private int ID_usuario;
 	private String senha;
 	
 	
-	public ClienteLogin ( ) {
-		
+	@Override
+	public String toString() {
+		return "ClienteLogin [ID_cliente=" + ID_cliente + ", Nome_Cliente=" + nome_Cliente + ", email_cliente="
+				+ email_cliente + "]";
 	}
-	
-   String Cadastrar() {	  
-	   return "ClienteLogin [Nome_Cliente=" + Nome_Cliente + ", ID_usuario=" + ID_usuario + ", senha=" + senha + "]";
-   }
-
-	
-	
-
-
 	public int getID_cliente() {
 		return ID_cliente;
 	}
-
-
 	public void setID_cliente(int iD_cliente) {
-		this.ID_cliente = iD_cliente;
+		ID_cliente = iD_cliente;
 	}
-
-
 	public String getNome_Cliente() {
-		return Nome_Cliente;
+		return nome_Cliente;
 	}
-
-
 	public void setNome_Cliente(String nome_Cliente) {
-		this.Nome_Cliente = nome_Cliente;
+		nome_Cliente = nome_Cliente;
 	}
-
-
 	public String getCPF_cliente() {
 		return CPF_cliente;
 	}
-
-
-	public void setCPF_cliente(String CPF_cliente) {
-		this.CPF_cliente = CPF_cliente;
+	public void setCPF_cliente(String cPF_cliente) {
+		CPF_cliente = cPF_cliente;
 	}
-	
 	public String getRG_cliente() {
 		return RG_cliente;
 	}
-
 	public void setRG_cliente(String rG_cliente) {
 		RG_cliente = rG_cliente;
 	}
-
-
-
-	public String getTelefone() {
-		return telefone;
+	public String getEmail_cliente() {
+		return email_cliente;
 	}
-
-
-	public void setTelefone(String telefone) {
-		this.telefone = telefone;
+	public void setEmail_cliente(String email_cliente) {
+		this.email_cliente = email_cliente;
 	}
-
-
+	public String getTelefone_Cliente() {
+		return telefone_Cliente;
+	}
+	public void setTelefone_Cliente(String telefone_Cliente) {
+		this.telefone_Cliente = telefone_Cliente;
+	}
 	public Date getData_Nascimento() {
 		return data_Nascimento;
 	}
-
-
 	public void setData_Nascimento(Date data_Nascimento) {
 		this.data_Nascimento = data_Nascimento;
 	}
-
-
-	public int getID_usuario() {
-		return ID_usuario;
-	}
-
-
-	public void setID_usuario(int iD_usuario) {
-		this.ID_usuario = iD_usuario;
-	}
-
-
 	public String getSenha() {
 		return senha;
 	}
-
-
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
-
-
-	
-
 }
